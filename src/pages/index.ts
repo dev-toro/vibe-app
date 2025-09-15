993397d7d463d4effb616d8cfe13edadaaeabe7c
dev-toro <d.toro@celonis.com>
2025-09-15 15:18:43 +0200

    Updates

--- src/pages/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as Playground } from './Playground';
-export { default as Libraries } from './Libraries';
-export { default as Marketplace } from './Marketplace';
-export { default as Runtimes } from './Runtimes';
-export { default as Workloads } from './Workloads';
-export { default as DataIngestion } from './DataIngestion';
